3f94aefad12fa14b61a837c0d0377b3cc41eb5ac
Bhanu <Bhanu@ZEAdmins-MBP.lan>
2024-12-13 21:59:30 +0530

    First version of the project- changes

---
+++ details.js
@@ -0,0 +1,76 @@
+const ACCESS_KEY = "LZhLS7xQShAUk1actF6WieRA17dVf1HZ5DPFTAY7k8Y";
+let firstImage = "";
+let thumbnailsArray = [];
+let unsplashImages = [];
+let firstImageElement = document.getElementById("main-image");
+
+async function fetchUnsplashApiPhotos() {
+  try {
+    const res = await fetch(
+      `https://api.unsplash.com/photos/random?count=5&client_id=${ACCESS_KEY}`
+    );
+    const unsplashPhotosList = await res.json();
+    console.log(unsplashPhotosList);
+    unsplashImages = unsplashPhotosList.map((photo) => photo.urls.regular);
+    firstImage = unsplashPhotosList && unsplashPhotosList[0].urls.regular;
+    fetchLoremPicsumApiPhotos();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function fetchLoremPicsumApiPhotos() {
+  try {
+    const res = await fetch("https://picsum.photos/v2/list?page=1&limit=5");
+    const loremPhotosList = await res.json();
+    thumbnailsArray = loremPhotosList.map((photo) => photo.download_url);
+    console.log(loremPhotosList);
+    loadCarousel(firstImage, thumbnailsArray);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function onClickNext() {
+  let currentIndex = unsplashImages.indexOf(firstImageElement.src);
+  currentIndex =
+    currentIndex === unsplashImages.length - 1 ? 0 : currentIndex + 1;
+  firstImageElement.src = unsplashImages[currentIndex];
+}
+
+function onClickPrev() {
+  let currentIndex = unsplashImages.indexOf(firstImageElement.src);
+  currentIndex =
+    currentIndex === 0 ? unsplashImages.length - 1 : currentIndex - 1;
+  firstImageElement.src = unsplashImages[currentIndex];
+}
+
+function changeToMainImage(key) {
+  firstImageElement.src = unsplashImages[key] || unsplashImages[0];
+}
+
+function loadCarousel(main, thumbnailImagesList) {
+  firstImageElement.src = main;
+
+  const thumbnailsContainer = document.getElementById("thumbnail-images");
+  thumbnailsContainer.innerHtml = "";
+
+  thumbnailImagesList.forEach((img, key) => {
+    const thumbnailImage = document.createElement("div");
+    thumbnailImage.classList.add("thumbnail");
+    thumbnailImage.classList.add("pointer");
+    thumbnailImage.id = `thumbnail-img-${key}`;
+
+    const imgElement = document.createElement("img");
+    imgElement.src = img;
+    imgElement.alt = `thumbnail ${key}`;
+
+    thumbnailImage.onclick = () => changeToMainImage(key);
+
+    thumbnailImage.appendChild(imgElement);
+
+    thumbnailsContainer.appendChild(thumbnailImage);
+  });
+}
+
+fetchUnsplashApiPhotos();
